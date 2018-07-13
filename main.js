3f4fae765fa5c86c9c0498605479524079177e82
wongthebombdotcom <40044966+wongthebombdotcom@users.noreply.github.com>
2018-07-12 17:59:16 -0800

    trying to still get names up

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-    //This is from airtable authenticaiton
-    var airtable_list_url = 'https://api.airtable.com/v0/appzuVq4QVRYlpDfV/Table%201?api_key=key45t6DMFSUQleHA'
+// This urls is from airtable from the Authentication section
+var airtable_list_url = 'https://api.airtable.com/v0/appzuVq4QVRYlpDfV/Table%201?api_key=key45t6DMFSUQleHA';
 
-    //This is where we get the JSON data from airtable
-    $.getJSON( airtable_list_url, function( data ) {
-        var items = [];
-        $.each( data.records, function( key, val ) {
-          console.log(val.fields)
-          items.push(`<h2>${val.fields['Name']}</h2>`);
-        });
-        $(".list-view").append(items.join(''));
-      });
+// This is where we get the JSON data from Airtable!
+
+$.getJSON( airtable_list_url, function( data ) {
+  var items = [];
+  $.each( data.records, function( key, val ) {
+    console.log(val.fields)
+    items.push(`<h2>${val.fields['Name']}</h2>`);
+  });
+  $(".list-view").append(items.join(''));
+});
+
