4e30dd0994f39b03dca216d7f2905c2ea05e99ea
wongthebombdotcom <40044966+wongthebombdotcom@users.noreply.github.com>
2018-07-19 14:58:40 -0800

    hgh

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // This urls is from airtable from the Authentication section
-var airtable_list_url = 'https://api.airtable.com/v0/appzuVq4QVRYlpDfV/Table%201?api_key=key45t6DMFSUQleHA';
+var airtable_list_url = 'https://api.airtable.com/v0/appzuVq4QVRYlpDfV/Chicken%20Recipes?api_key=key87Kh1djUSEKJz9';
 
 // This is where we get the JSON data from Airtable!
 
