e2dce15b0a4c86c037cbb7170593492f68bf677b
wongthebombdotcom <40044966+wongthebombdotcom@users.noreply.github.com>
2018-08-09 18:00:41 -0700

    Almost have list view working and got a restaurant section that does not work

---
+++ Chicken Mania/main.js
@@ -0,0 +1,4 @@
+// This urls is from airtable from the Authentication section
+var airtable_list_url = 'https://api.airtable.com/v0/appzuVq4QVRYlpDfV/Chicken%20Recipes?api_key=key87Kh1djUSEKJz9';
+
+// This is where we get the JSON data from Airtable!
