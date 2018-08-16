4f63e7649a9aa4eba8628a631d40b50e6ea53ea2
wongthebombdotcom <40044966+wongthebombdotcom@users.noreply.github.com>
2018-08-16 04:26:15 -0700

    FINALLLY GOT RESTAURANTS TO WORK!!!!

--- Chicken Mania/restaurant.js
+++ Chicken Mania/restaurant.js
@@ -11,15 +11,15 @@ var getParameterByName = function (name, url) {
 var api_key = 'key87Kh1djUSEKJz9';
 
 // Template that generates the HTML for one item in our list view, given the parameters passed in
-var listView = function (id, name, image, rating, pricerange) {
-  return `<div class="col-sm-3">
+var listView = function(id, name, pictureUrl, rating, pricerange) {
+  return `<div class="col-sm-6">
     <div class="card mb-4 box-shadow">
-      <a href="index.html?id=${id}"><img class="card-img-top" src="${image}"></a>
+      <a href="restaurant.html?id=${id}"><img class="card-img-top" src="${pictureUrl}"></a>
       <div class="card-body">
-        <h4><a href="index.html?id=${id}">${name}</a></h4>
+        <h4><a href="restaurant.html?id=${id}">${name}</a></h4>
         <div class="d-flex justify-content-between align-items-center">
-          <small class="text-muted">${pricerange}</small>
           <small class="text-muted">${rating}</small>
+          <small class="text-muted" id="price">${pricerange}</small>
           </div>
       </div>
     </div>
@@ -27,75 +27,83 @@ var listView = function (id, name, image, rating, pricerange) {
 };
 
 // Get and display the data for all items
-var getDataForList = function () {
+var getDataForList = function() {
   // 1. Gets the data from the Airtable API
-  $.getJSON(`https://api.airtable.com/v0/appzuVq4QVRYlpDfV/Chicken%20Places?api_key=${api_key}`, function (data) {
-    // console.log(data.records);
-    var html = [];
-    html.push(`<div class="row">`);
-    // 2. Iterates over every record and uses the list template
-    $.each(data.records, function (index, val) {
-      // console.log(val.fields)
-      var id = val.id;
-      var fields = val.fields;
-      var name = fields["Name"];
-      var image = val.fields['Image'][0] ? val.fields['Image'][0].url : null;
-      var description = fields["Rating"];
-      var itemHTML = listView(id, name, image, description);
-      html.push(itemHTML);
-    });
-    html.push(`</div>`);
-    // 3. Adds HTML for every item to our page
-    $(".list-view").append(html.join(""));
-  });
-}
+  $.getJSON(
+    `https://api.airtable.com/v0/appzuVq4QVRYlpDfV/Chicken%20Places?api_key=${api_key}`,
+    function(data) {
+      // console.log(data);
+      var html = [];
+      html.push(`<div class="row">`);
+      // 2. Iterates over every record and uses the list template
+      $.each(data.records, function(index, record) {
+        // console.log(record)
+        var id = record.id;
+        var fields = record.fields;
+        var name = fields["Name"];
+        var image = fields["Image"] ? fields["Image"][0].url : "";
+        var pricerange = fields["Price Range"];
+        var rating = fields["Rating"];
+        // Pass all fields into the List Template
+        var itemHTML = listView(id, name, image, rating, pricerange);
+        html.push(itemHTML);
+      });
+      html.push(`</div>`);
+      // 3. Adds HTML for every item to our page
+      $(".list-view").append(html.join(""));
+    },
+  );
+};
 
 // Template that generates HTML for one item in our detail view, given the parameters passed in
-var detailView = function (id, name, pictureUrl, pricerange, description, website, rating, address, number) {
-  return `<div class="col-sm-10">
-      <div class="card mb-4 box-shadow">
-        <img class="card-img-top" src="${pictureUrl}">
-        <div class="card-body">
-          <h2>${name}</h2>
-          <p class="card-text">${pricerange}</p>
-          <p class="card-text">${rating}</p>
-          <p class="card-text">${address}</p>
-          <div class="d-flex justify-content-between align-items-center">
-            <small class="text-muted">${description}</small>
-          </div>
-          ${website ? `<a href="${website}">${website}</a>` : ``}
-          ${number ? `<a href="${number}">${number}</a>` : ``}
-          <hr/>
+var detailView = function(id, name, pictureUrl, rating, pricerange, address, website, description, number) {
+  return `<div class="col-sm-12">
+    <div class="card mb-6 box-shadow">
+      <img class="card-img-top" src="${pictureUrl}">
+      <div class="card-body">
+        <h2>${name}</h2>
+        <p class="card-text">${rating}</p>
+        <p class="card-text">${pricerange}</p>
+        <p class="card-text">${address}</p>
+        ${description ? `<p class="card-text">${description}</p>` : ``}
+        <div class="d-flex justify-content-between align-items-center">
         </div>
+        ${website ? `<a href="${website}">${website}</a>` : ``}
+        <hr />
+        <a href="https://www.google.com/maps/search/${name}"> Find Location </a>
       </div>
-    </div>`;
-}
-
+    </div>
+  </div>`;
+};
 
 // Get and display the data for one item based on on the ID
-var getDataForId = function (id) {
-  $.getJSON(`https://api.airtable.com/v0/appzuVq4QVRYlpDfV/Chicken%20Places/${id}?api_key=${api_key}`, function (record) {
-    // console.log(data);
-    var html = [];
-    html.push(`<div class="row">`);
-    // console.log(val)
-    var id = record.id;
-    var fields = record.fields;
-    var name = fields["Name"];
-    var pictureUrl = fields["Pictures"] ? fields["Pictures"][0].url : '';
-    var address = fields["Address"];
-    var rating = fields["Rating"];
-    var description = fields["description"];
-    var website = fields["Website"];
-    var pricerange = fields["Price Range"];
-    var number = fields["Number"];
+var getDataForId = function(id) {
+  $.getJSON(`https://api.airtable.com/v0/appzuVq4QVRYlpDfV/Chicken%20Places/${id}?api_key=${api_key}`,
+    function(record) {
+      // console.log(data);
+      var html = [];
+      html.push(`<div class="row">`);
+      // console.log(record)
+      var id = record.id;
+      var fields = record.fields;
 
-    var itemHTML = detailView(id, name, pictureUrl, address, rating, description, website, pricerange, number);
-    html.push(itemHTML);
-    html.push(`</div>`);
-    $(".detail-view").append(html.join(""));
-  });
-}
+      var name = fields["Name"];
+      var image = fields["Image"] ? fields["Image"][0].url : "";
+      var pricerange = fields["Price Range"];
+      var rating = fields["Rating"];
+      var description = fields["Description"];
+      var website = fields["Website"];
+      var number = fields["Number"];
+      var address = fields["address"];
+
+      // Pass all fields into the Detail Template
+      var itemHTML = detailView(id, name, image, pricerange, rating, description, website, number, address);
+      html.push(itemHTML);
+      html.push(`</div>`);
+      $(".detail-view").append(html.join(""));
+    },
+  );
+};
 
 // Do we have an ID in the URL?
 var id = getParameterByName("id");
@@ -107,4 +115,3 @@ if (id) {
 } else {
   getDataForList();
 }
-
